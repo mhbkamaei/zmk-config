ad24605edc256289522741c613fbc6e3eb1c8174
mhbkamaei <mh.b.kamaei@gmail.com>
2024-10-01 16:15:41 +0330

    dfu.h

---
+++ config/dfu.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+// AdaFruit nrf52 Bootloader Specific. See
+// https://github.com/adafruit/Adafruit_nRF52_Bootloader/blob/eefbe6bda98b80cb296e2f1bb1c982d58fe4c12d/src/main.c#L110
+
+#define RST_DFU 0xA8
